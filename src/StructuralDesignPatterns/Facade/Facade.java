47b020ab8ffc2263284046c08578e46817384910
Faycal Kilali <53886202+faycalki@users.noreply.github.com>
2024-03-08 12:04:37 -0400

    Incomplete Facade Implementation

---
+++ src/StructuralDesignPatterns/Facade/Facade.java
@@ -0,0 +1,13 @@
+package StructuralDesignPatterns.Facade;
+
+public class Facade {
+    private SubsystemInterfaceOne systemOne;
+    private SubsystemInterfaceTwo systemTwo;
+    private SubsystemInterfaceThree systemThree;
+    private SubsystemInterfaceFour systemFour;
+    private AdditionalFacade additionalFacade;
+
+    public Facade(){
+
+    }
+}
